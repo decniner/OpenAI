d44030c460985f7883f65212e720d4ba037d06f3
Den <decniner@gmail.com>
2023-01-15 21:28:11 +0900

    Suppress dot output

--- CodeGenerator.ps1
+++ CodeGenerator.ps1
@@ -1,25 +1,35 @@
-# Store your API key in a variable
+function Generate-PasswordScript {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$apiKey
+    )
+    try {
+        # Define the endpoint URL and parameters for the request
+        $endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
+        $parameters = @{
+            "prompt" = "Write a PowerShell script that generates a random password with 8 characters"
+            "max_tokens" = 2048
+            "stop" = "`n"
+        }
+
+        # Add the API key to the headers of the request
+        $headers = @{
+            "Content-Type" = "application/json"
+            "Authorization" = "Bearer $apiKey"
+        }
+
+        # Make the request to the API and store the response in a variable
+        Invoke-RestMethod -Uri $endpoint -Method Post -Headers $headers -Body (ConvertTo-Json $parameters) -OutVariable response
+        $generatedCode = $response.choices[0].text
+
+        # Print the generated code
+        Write-Host $generatedCode
+    } catch {
+        Write-Error "An error occurred while trying to generate the password script: $($_.Exception.Message)"
+    }
+}
+
+# Call the function
 $apiKey = "YOUR_API_KEY"
-
-# Define the endpoint URL and parameters for the request
-$endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
-$parameters = @{
-    "prompt" = "Write a PowerShell script that generates a random password with 8 characters"
-    "max_tokens" = 2048
-    "stop" = "`n"
-}
-
-# Add the API key to the headers of the request
-$headers = @{
-    "Content-Type" = "application/json"
-    "Authorization" = "Bearer $apiKey"
-}
-
-# Make the request to the API
-$response = Invoke-RestMethod -Uri $endpoint -Method Post -Headers $headers -Body (ConvertTo-Json $parameters)
-
-# Store the generated code in a variable
-$generatedCode = $response.choices[0].text
-
-# Print the generated code
-Write-Host $generatedCode
+Generate-PasswordScript -apiKey $apiKey
