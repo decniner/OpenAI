7804725a5337239fd17a5337d06aac218301ede2
Den <decniner@gmail.com>
2023-01-15 21:44:13 +0900

    Accepts any prompt

--- CodeGenerator.ps1
+++ CodeGenerator.ps1
@@ -1,35 +1,32 @@
-function Generate-PasswordScript {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]$apiKey
-    )
-    try {
-        # Define the endpoint URL and parameters for the request
-        $endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
-        $parameters = @{
-            "prompt" = "Write a PowerShell script that generates a random password with 8 characters"
-            "max_tokens" = 2048
-            "stop" = "`n"
-        }
-
-        # Add the API key to the headers of the request
-        $headers = @{
-            "Content-Type" = "application/json"
-            "Authorization" = "Bearer $apiKey"
-        }
-
-        # Make the request to the API and store the response in a variable
-        Invoke-RestMethod -Uri $endpoint -Method Post -Headers $headers -Body (ConvertTo-Json $parameters) -OutVariable response
-        $generatedCode = $response.choices[0].text
-
-        # Print the generated code
-        Write-Host $generatedCode
-    } catch {
-        Write-Error "An error occurred while trying to generate the password script: $($_.Exception.Message)"
-    }
-}
-
-# Call the function
 $apiKey = "YOUR_API_KEY"
-Generate-PasswordScript -apiKey $apiKey
+$prompt = "Write a PowerShell script that generates a random password with 8 characters"
+$outputFile = "C:\path\to\file.ps1"
+
+try {
+    # Define the endpoint URL and parameters for the request
+    $endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
+    $parameters = @{
+        "prompt" = $prompt
+        "max_tokens" = 2048
+        "stop" = "`n"
+    }
+
+    # Add the API key to the headers of the request
+    $headers = @{
+        "Content-Type" = "application/json"
+        "Authorization" = "Bearer $apiKey"
+    }
+
+    # Make the request to the API and store the response in a variable
+    Invoke-RestMethod -Uri $endpoint -Method Post -Headers $headers -Body (ConvertTo-Json $parameters) -OutVariable response
+    $generatedCode = $response.choices[0].text
+
+    # Print the generated code
+    Write-Host $generatedCode
+
+    # Write the generated code to a file
+    $generatedCode | Out-File $outputFile
+
+} catch {
+    Write-Error "An error occurred while trying to generate the script: $($_.Exception.Message)"
+}
