343a5b22c0027a0b279fc30c7d131942d7c23002
Den <decniner@gmail.com>
2023-01-16 00:22:31 +0900

    Fixed

--- CodeGenerator.ps1
+++ CodeGenerator.ps1
@@ -1,17 +1,17 @@
 function _generate-PSScript{
 
     [CmdletBinding()]
-        param (
-            [Parameter(Mandatory = $true)]
-            [string]$prompt
-        )
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$prompt
+    )
 
-    $apiKey = "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"
+    $apiKey = "sk-n0DNx0fYTxlVO3WmlKDxT3BlbKFJLbGrmvX8bDd93CVfgHSG"
 
     # Define the endpoint URL and parameters for the request
     $endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
     $parameters = @{
-        "prompt" = "Write a powershell script to get the current time and display it on screen"
+        "prompt" = $prompt
         "max_tokens" = 1024
         #"stop" = "`n"
     }
@@ -23,9 +23,10 @@ function _generate-PSScript{
     }
 
     # Make the request to the API and store the response in a variable
-    Invoke-RestMethod -Uri $endpoint -Method Post -Headers $headers -Body (ConvertTo-Json $parameters) -OutVariable response | Out-Null
+    $response = Invoke-RestMethod -Uri $endpoint -Method Post -Headers $headers -Body (ConvertTo-Json $parameters)
     $generatedCode = $response.choices.text
 
     # Print the generated code
     Write-Host $generatedCode
 }
+
