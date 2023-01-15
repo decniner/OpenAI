a4dea0f5c0a28e1ebea5394bdb77b95dcaf8b6d8
Den <decniner@gmail.com>
2023-01-15 22:35:55 +0900

    Simplified

--- CodeGenerator.ps1
+++ CodeGenerator.ps1
@@ -1,52 +1,31 @@
-function Generate-Script {
+function _generate-PSScript{
+
     [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]$apiKey,
-        [Parameter(Mandatory = $true)]
-        [string]$prompt,
-        [string]$filePath = ""
-    )
-    try {
-        # Define the endpoint URL and parameters for the request
-        $endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
-        $parameters = @{
-            "prompt" = $prompt
-            "max_tokens" = 2048
-            "stop" = "`n"
-        }
+        param (
+            [Parameter(Mandatory = $true)]
+            [string]$prompt
+        )
 
-        # Add the API key to the headers of the request
-        $headers = @{
-            "Content-Type" = "application/json"
-            "Authorization" = "Bearer $apiKey"
-        }
+    $apiKey = "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"
 
-        # Make the request to the API and store the response in a variable
-        Invoke-RestMethod -Uri $endpoint -Method Post -Headers $headers -Body (ConvertTo-Json $parameters) -OutVariable response
-        $generatedCode = $response.choices[0].text
-
-        # Print the generated code
-        Write-Host $generatedCode
-
-        # Write the code to a file
-        if($filePath -ne ""){
-            $generatedCode | Out-File $filePath -Force
-        Write-Host "Script saved to $filePath"
+    # Define the endpoint URL and parameters for the request
+    $endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
+    $parameters = @{
+        "prompt" = "Write a powershell script to get the current time and display it on screen"
+        "max_tokens" = 1024
+        #"stop" = "`n"
     }
-} catch {
-    Write-Error "An error occurred while trying to generate the script: $($_.Exception.Message)"
-}
-}
 
-#Call the function
-$apiKey = "YOUR_API_KEY"
-$prompt = "Write a PowerShell script that generates a random password with 8 characters"
-$filePath = "C:\path\to\file.ps1"
-Generate-Script -apiKey $apiKey -prompt $prompt -filePath $filePath
+    # Add the API key to the headers of the request
+    $headers = @{
+        "Content-Type" = "application/json"
+        "Authorization" = "Bearer $apiKey"
+    }
 
-<#
-This version of the script includes a new parameter `$filePath` which is optional, you can use it to specify the path and name of the file where you want to save the generated code. If you don't provide a value for this parameter, the script will only display the code on the console. Also, the `Out-File` cmdlet has the `-Force` parameter that is used to overwrite the file if it already exists, otherwise you will get an error.
-Please remember to replace the YOUR_API_KEY with your actual API key that you got from OpenAI in the script.
-It's important to review the generated code carefully and test it properly before using it in production, also, make sure to follow the OpenAI's terms of service and guidelines when using GPT-3 API.
-#>
+    # Make the request to the API and store the response in a variable
+    Invoke-RestMethod -Uri $endpoint -Method Post -Headers $headers -Body (ConvertTo-Json $parameters) -OutVariable response | Out-Null
+    $generatedCode = $response.choices.text
+
+    # Print the generated code
+    Write-Host $generatedCode
+}
